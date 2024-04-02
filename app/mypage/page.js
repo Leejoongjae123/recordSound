c8af63ae12acb8dddf9097c9d371e72c90ec6e0e
ljj33 <ljj3347@naver.com>
2024-04-03 01:11:24 +0900

    first commit

---
+++ app/mypage/page.js
@@ -0,0 +1,89 @@
+"use client";
+import React, { useState } from "react";
+import { Radio, Space, Table, Tag } from "antd";
+
+const columns = [
+  {
+    title: "Name",
+    dataIndex: "name",
+    key: "name",
+    render: (text) => <a>{text}</a>,
+  },
+
+  {
+    title: "Address",
+    dataIndex: "address",
+    key: "address",
+  },
+  {
+    title: "Tags",
+    key: "tags",
+    dataIndex: "tags",
+    render: (tags) => (
+      <span>
+        {tags.map((tag) => {
+          let color = tag.length > 5 ? "geekblue" : "green";
+          if (tag === "loser") {
+            color = "volcano";
+          }
+          return (
+            <Tag color={color} key={tag}>
+              {tag.toUpperCase()}
+            </Tag>
+          );
+        })}
+      </span>
+    ),
+  },
+  {
+    title: "Action",
+    key: "action",
+    render: (_, record) => (
+      <Space size="middle">
+        <a>Invite {record.name}</a>
+        <a>Delete</a>
+      </Space>
+    ),
+  },
+];
+const data = [
+  {
+    key: "1",
+    name: "John Brown",
+    age: 32,
+    address: "New York No. 1 Lake Park",
+    tags: ["nice", "developer"],
+  },
+  {
+    key: "2",
+    name: "Jim Green",
+    age: 42,
+    address: "London No. 1 Lake Park",
+    tags: ["loser"],
+  },
+  {
+    key: "3",
+    name: "Joe Black",
+    age: 32,
+    address: "Sydney No. 1 Lake Park",
+    tags: ["cool", "teacher"],
+  },
+];
+const App = () => {
+  const [top, setTop] = useState("topLeft");
+  const [bottom, setBottom] = useState("bottomRight");
+  return (
+    <div style={{ display: "flex", width: "100%", height: "auto",justifyContent:'center',alignItems:'center',padding:"10% 20% 10% 20%"}}>
+      <div style={{ width: "100%",margin:'auto'}}>
+        <Table
+          columns={columns}
+          pagination={{
+            position: [bottom],
+          }}
+          dataSource={data}
+        />
+      </div>
+    </div>
+  );
+};
+export default App;
---
+++ app/upload/page.js
@@ -0,0 +1,35 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div className="spark-section-5">
+    <div className="spark-container-5 w-container">
+      <div className="spark-centered-901">
+        <h2>Upload</h2>
+        <p className="spark-foreground-secondary-2">감성 디지털화 서비스는 당신의 말과 글을 넘어서는 경험을 제공합니다. 간직하고 싶은 그 순간, 그 감정을 텍스트로 전달하세요.</p>
+      </div>
+      <div className="spark-regular-form-2 w-form">
+        <form id="General-Contact-Form" name="wf-form-General-Contact-Form" data-name="General Contact Form" method="get" data-wf-page-id="660c1c6d3dc8e9d96341e88b" data-wf-element-id="31ac8418-3dcd-bf27-f246-6c316d20204b"><label for="General-Contact-Form---Name">Name</label><input className="spark-input-5 w-input" maxlength="256" name="General-Contact-Form---Name" data-name="General Contact Form - Name" placeholder="Enter Your Name" type="text" id="General-Contact-Form---Name" required=""/><label for="General-Contact-Form---Message">Message</label><textarea id="General-Contact-Form---Message" name="General-Contact-Form---Message" maxlength="5000" data-name="General Contact Form - Message" placeholder="Enter your message" className="spark-input-5 spark-message-input w-input"></textarea><label for="select-1">대분류</label><select id="select-1" name="select-1" data-name="select-1" className="sysf-select sysf-medium w-select">
+            <option value="">Select one...</option>
+            <option value="First">First Choice</option>
+            <option value="Second">Second Choice</option>
+            <option value="Third">Third Choice</option>
+          </select><label for="General-Contact-Form---Message-2">소분류</label><select id="select-1" name="select-1" data-name="select-1" className="sysf-select sysf-medium w-select">
+            <option value="">Select one...</option>
+            <option value="First">First Choice</option>
+            <option value="Second">Second Choice</option>
+            <option value="Third">Third Choice</option>
+          </select><input type="submit" data-wait="Please wait..." className="spark-button-5 spark-full-width w-button" value="Record"/><input type="submit" data-wait="Please wait..." className="spark-button-5 spark-full-width w-button" value="Submit"/></form>
+        <div className="spark-form-success-4 w-form-done">
+          <div>Thank you! Your submission has been received!</div>
+        </div>
+        <div className="w-form-fail">
+          <div>Oops! Something went wrong while submitting the form.</div>
+        </div>
+      </div>
+    </div>
+  </div>
+  )
+}
+
+export default page
